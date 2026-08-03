a81eefceeb814df4bf4ac3761077c8de286d831f
Luca29499 <k5g6mw4hwg@privaterelay.appleid.com>
2026-08-03 11:36:21 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "kilometer-null-flat-v1";
+const CACHE = "kilometer-null-flat-v6";
 const FILES = ["./", "./index.html", "./app.js", "./styles.css", "./manifest.webmanifest", "./icon.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
